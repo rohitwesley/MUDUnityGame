9efc5804d73d27275d349967b3b5c4d35e3b3d3f
Wesley <wesleythomas360@gmail.com>
2020-04-15 09:43:02 -0700

    cleaned the scene setup basic lighting rig

---
+++ Assets/MUD/Scripts/GameState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameLogic;
+using TMPro;
+
+public class GameState : MonoBehaviour
+{
+    [Tooltip("Pick Up UI Text")]
+    [SerializeField] private TextMeshProUGUI scoreWidgetText;
+    [SerializeField] private TextMeshProUGUI pickupWidgetText;
+    [SerializeField] private List<Agents> units;
+
+    public static int pickUpsCount = 0;
+    public static int pickUpsTotal = 3;
+    void Update()
+    {
+        foreach (Agents agent in units)
+        {
+            if(agent.GetAgentName() == "Player")
+            {
+                scoreWidgetText.text = "Score: " + agent.scoreValue;
+                pickupWidgetText.text = "Loot: " + pickUpsCount;
+            }
+        }
+
+    }
+
+}
